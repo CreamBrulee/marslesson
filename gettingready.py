ac5d4aecdbfc77e9497e6ef7fe704486291eb008
CreamBrulee <mariakon1404@gmail.com>
2023-03-01 23:42:44 +0300

    список профессий

--- gettingready.py
+++ gettingready.py
@@ -13,5 +13,17 @@ def training(prof):
     return render_template('training.html', title=prof)
 
 
+@app.route('/list_prof/<list>')
+def list_prof(list):
+    proflist = ['инженер-исследователь', 'пилот', 'строитель',
+                'экзобиолог', 'врач', 'инженер по терраформированию',
+                'климатолог', 'специалист по радиационной защите',
+                'астрогеолог', 'гляциолог', 'инженер жизнеобеспечения',
+                'метеоролог', 'оператор марсохода', 'киберинженер',
+                'штурман', 'пилот дронов']
+    return render_template('list_prof.html', title=f"127.0.0.1:8080/list_prof/{list}", list0=list,
+                           proflist=proflist)
+
+
 if __name__ == '__main__':
     app.run(port=8080, host='127.0.0.1')
